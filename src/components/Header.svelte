e7fb4b6bad2f47927cdb7b89dbe7320de929d1c2
Cam Crow <19ccrow99@gmail.com>
2024-01-21 14:55:15 -0600

    style: added stone, green, amber

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,16 +2,16 @@
 </script>
 
 <div
-	class="flex items-center justify-between flex-wrap bg-stone-700 p-8 mb-4 text-lg sticky top-0 z-50 rounded-b-lg"
+	class="flex items-center justify-between flex-wrap bg-stone-800 p-8 mb-4 text-xl font-bold sticky top-0 z-50 border-b border-amber-600"
 >
-	<div class="flex flex-row gap-x-4">
-		<h3>Sudoku+</h3>
-		<div>Puzzles</div>
-		<div class="hidden md:flex">Editor</div>
-		<div>About</div>
+	<div class="flex flex-row gap-x-6">
+		<h3 class="hover:text-green-500">Sudoku+</h3>
+		<div class="hover:text-green-500">Puzzles</div>
+		<div class="hidden md:flex hover:text-green-500">Editor</div>
+		<div class="hover:text-green-500">About</div>
 	</div>
-	<div class="flex flex-row gap-x-4">
-		<div>Log In</div>
-		<div>Sign Up</div>
+	<div class="flex flex-row gap-x-6">
+		<div class="hover:text-green-500">Log In</div>
+		<div class="hover:text-green-500">Sign Up</div>
 	</div>
 </div>
