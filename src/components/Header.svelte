fa097b8c21618825a370f2bba95620f0da22f0c7
Cam Crow <19ccrow99@gmail.com>
2024-01-23 12:13:18 -0600

    feat: modal animation and modal store

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,35 +1,63 @@
 <script lang="ts">
 	import NavLink from './NavLink.svelte';
 	import 'iconify-icon';
+	import { quintOut } from 'svelte/easing';
+	import { slide, fade } from 'svelte/transition';
+	import { showModal } from '../stores/modalStore';
 
 	const links = [
 		['Puzzles', '/puzzles'],
 		['Editor', '/editor'],
 		['About', '/about']
 	];
-
 </script>
 
-<div
-	class="flex items-center justify-between flex-wrap bg-paper p-6 mb-4 text-xl font-bold sticky top-0 z-50 border-b border-carbon-200"
->
-	<div class="flex flex-row gap-x-2">
-		<iconify-icon icon="mingcute:menu-fill" class="ml-2 mr-2 pt-3 scale-150 md:hidden" />
-		<a href="/" class="pt-2 pb-2 pl-2 pr-2 scale-125 mr-4 ml-4 bg-sky-300">Variance</a>
-		{#each links as link}
-			<NavLink href={link[1]} text={link[0]} />
-		{/each}
-	</div>
-	<div class="flex-row hidden sm:flex">
-		<div
-			class="hover:text-watermelon hover:underline decoration-watermelon hover:bg-carbon-200 pt-2 pb-2 pl-4 pr-4 rounded-3xl hover:bg-opacity-5 transition"
-		>
-			Log In
+<div class={$showModal ? 'flex flex-col h-screen' : ''}>
+	<div
+		class="flex items-center justify-between flex-wrap bg-paper p-6 text-xl font-bold sticky top-0 z-50 {$showModal
+			? 'border-none'
+			: 'border-b border-carbon-200'}"
+	>
+		<div class="flex flex-row gap-x-2">
+			<button
+				on:click={(e) => {
+					showModal.set(!$showModal);
+				}}
+			>
+				<iconify-icon
+					icon="mingcute:menu-fill"
+					class="mr-3 pt-1 scale-150 md:hidden hover:text-watermelon hover:cursor-pointer"
+				/>
+			</button>
+			<a
+				href="/"
+				class="pt-2 pb-2 pl-2 pr-2 scale-125 mr-4 ml-4 hover:text-paper hover:cursor-pointer transition-all {$showModal
+					? 'hover:text-watermelon hover:underline decoration-watermelon'
+					: 'hover:bg-watermelon hover:text-paper bg-sky-300'}">Variance</a
+			>
+			{#each links as link}
+				<NavLink href={link[1]} text={link[0]} />
+			{/each}
 		</div>
-		<div
-			class="hover:text-watermelon hover:underline decoration-watermelon hover:bg-carbon-200 pt-2 pb-2 pl-4 pr-4 rounded-3xl hover:bg-opacity-5 transition"
-		>
-			Sign Up
+		<div class="flex-row hidden sm:flex">
+			<div
+				class="hover:text-watermelon hover:underline decoration-watermelon hover:bg-carbon-200 pt-2 pb-2 pl-4 pr-4 rounded-3xl hover:bg-opacity-5 transition"
+			>
+				Log In
+			</div>
+			<div
+				class="hover:text-watermelon hover:underline decoration-watermelon hover:bg-carbon-200 pt-2 pb-2 pl-4 pr-4 rounded-3xl hover:bg-opacity-5 transition"
+			>
+				Sign Up
+			</div>
 		</div>
 	</div>
+	{#if $showModal}
+		<div
+			class="grid w-full h-full bg-watermelon overflow-hidden"
+			transition:slide={{ delay: 0, duration: 400, easing: quintOut, axis: 'y' }}
+		>
+			Hello!
+		</div>
+	{/if}
 </div>
